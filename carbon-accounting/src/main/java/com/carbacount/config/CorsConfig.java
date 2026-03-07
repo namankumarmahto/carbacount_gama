52ab095000de2bae72617895d7028a8cb97e348a
Naman Kumar Mahto <mahtokumarnaman@gmail.com>
2026-03-08 03:21:57 +0530

    added frontend url

---
+++ carbon-accounting/src/main/java/com/carbacount/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.carbacount.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://carbacount-gama-gamma.vercel.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
